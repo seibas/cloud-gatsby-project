a3a83a2500d7b3f8789e672ba185b5275170658c
sebastiano <36180003+seibas@users.noreply.github.com>
2021-03-04 01:30:30 +0900

    ad ptag the data

--- src/templates/work.js
+++ src/templates/work.js
@@ -9,9 +9,12 @@ export default ({ data }) => (
   <Layout>
     <article className="sheet">
       <HelmetDatoCms seo={data.datoCmsWork.seoMetaTags} />
-      <div className="sheet__inner">
-        <h1 className="sheet__title">{data.datoCmsWork.title}</h1>
+      <div className="sheet__inner"> 
+   
+        <h1 className="sheet__title">{data.datoCmsWork.title}</h1>     
+        <p className="sheet__lead">{data.datoCmsWork.date}</p>
         <p className="sheet__lead">{data.datoCmsWork.excerpt}</p>
+     
         <div className="sheet__slider">
           <Slider infinite={true} slidesToShow={2} arrows>
             {data.datoCmsWork.gallery.map(({ fluid }) => (
